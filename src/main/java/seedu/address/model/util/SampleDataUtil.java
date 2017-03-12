73a2adde7a181db18701b0f5288b40bb8a9bf5f6
Bennett Goh <bennettgoh@gmail.com>
2017-03-12 20:46:10 +0800

    fixed data util by adding one new dummy task

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -15,24 +15,9 @@ public class SampleDataUtil {
     public static Task[] getSamplePersons() {
         try {
             return new Task[] {
-                new Task(new Name("Alex Yeoh"), new Description("87438807"), new StartTime("alexyeoh@gmail.com"),
-                    new EndTime("Blk 30 Geylang Street 29, #06-40"),
-                    new UniqueTagList("friends")),
-                new Task(new Name("Bernice Yu"), new Description("99272758"), new StartTime("berniceyu@gmail.com"),
-                    new EndTime("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    new UniqueTagList("colleagues", "friends")),
-                new Task(new Name("Charlotte Oliveiro"), new Description("93210283"), new StartTime("charlotte@yahoo.com"),
-                    new EndTime("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    new UniqueTagList("neighbours")),
-                new Task(new Name("David Li"), new Description("91031282"), new StartTime("lidavid@google.com"),
-                    new EndTime("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    new UniqueTagList("family")),
-                new Task(new Name("Irfan Ibrahim"), new Description("92492021"), new StartTime("irfan@outlook.com"),
-                    new EndTime("Blk 47 Tampines Street 20, #17-35"),
-                    new UniqueTagList("classmates")),
-                new Task(new Name("Roy Balakrishnan"), new Description("92624417"), new StartTime("royb@gmail.com"),
-                    new EndTime("Blk 45 Aljunied Street 85, #11-31"),
-                    new UniqueTagList("colleagues"))
+                new Task(new Name("buy milk"), new Description("87438807"), new StartTime(""),
+                    new EndTime("2017-03-04-1000"), new ID("1"), new Priority("h"), new Status("undone"),
+                    new UniqueTagList("friends"))
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
