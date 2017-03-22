65d587f3c3654f3fcadc74383778d19088c4fa71
EDWARD <E0003394@u.nus.edu>
2017-03-23 00:34:30 +0800

    1. update find function to search undone task by default. 2. implemented find @keyword to search archived tasks

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,92 +1,111 @@
 package seedu.address.logic.commands;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Assert;
 
 import seedu.address.model.Task.Priority;
 import seedu.address.model.Task.Status;
+import seedu.address.model.tag.Tag;
+
 /**
- * Finds and lists all tasks in task manager whose name contains any of the argument keywords.
- * Keyword matching is case sensitive.
+ * Finds and lists all tasks in task manager whose name contains any of the
+ * argument keywords. Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
 
-    public static final String COMMAND_WORD = "find";
-    private static final String HIGH_PRIORITY = "h";
-    private static final String MEDIUM_PRIORITY = "m";
-    private static final String LOW_PRIORITY = "l";
+	public static final String COMMAND_WORD = "find";
+	private static final String HIGH_PRIORITY = "h";
+	private static final String MEDIUM_PRIORITY = "m";
+	private static final String LOW_PRIORITY = "l";
 	private static final Object DONE = "done";
 	private static final Object UNDONE = "undone";
-	
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
-            + "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
-            + "When prefix (#) is used, will display all task with the associated status or priority level.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " study meeting friend\n"
-            + "Parameters: #KEYWORD \n"
-            + "Example: " + COMMAND_WORD + " #h\n";
 
-    private final Set<String> keywords;
-    
-    private final Status status;
-    
-    private final Priority priority;
+	public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all tasks whose names contain any of "
+			+ "the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+			+ "When prefix (#) is used, will display all task with the associated status or priority level.\n"
+			+ "Parameters: KEYWORD [MORE_KEYWORDS]...\n" + "Example: " + COMMAND_WORD + " study meeting friend\n"
+			+ "Parameters: #KEYWORD \n" + "Example: " + COMMAND_WORD + " #h\n";
 
-    public FindCommand(Set<String> keywords) {
-        this.keywords = keywords;
-        this.status = null;
-        this.priority = null;
-    }
+	private final Set<String> keywords;
 
-    public FindCommand(Status status) {
-        this.keywords = null;
-        this.status = status;
-        this.priority = null;
-    }
-    
-    public FindCommand(Priority priority) {
-        this.keywords = null;
-        this.status = null;
-        this.priority = priority;
-    }
-    
-    @Override
-    public CommandResult execute() {
-    	if(this.keywords!=null){
-    		model.updateFilteredTaskListByKeywords(keywords);
-    	}
-    	else if(this.status!=null){
-    		updateByStatusLevel(status);
-    	}
-    	else if(this.priority != null){
-    		updateByPriorityLevel(priority);
-    	}
-    	return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
-    }
+	private final Status status;
+
+	private final Priority priority;
+
+	private final String archive;
+
+	public FindCommand(Set<String> keywords) {
+		this.keywords = keywords;
+		this.status = null;
+		this.priority = null;
+		this.archive = null;
+	}
+
+	public FindCommand(String keyword) {
+		this.keywords = null;
+		this.status = null;
+		this.priority = null;
+		this.archive = keyword;
+	}
+
+	public FindCommand(Status status) {
+		this.keywords = null;
+		this.status = status;
+		this.priority = null;
+		this.archive = null;
+	}
+
+	public FindCommand(Priority priority) {
+		this.keywords = null;
+		this.status = null;
+		this.priority = priority;
+		this.archive = null;
+	}
+
+	@Override
+	public CommandResult execute() {
+		if (this.keywords != null) {
+			updateByKeywords();
+		} else if (this.status != null) {
+			updateByStatusLevel(status);
+		} else if (this.priority != null) {
+			updateByPriorityLevel(priority);
+		} else if (this.archive != null) {
+			updateByArchivedKeyword();
+		}
+		return new CommandResult(getMessageForTaskListShownSummary(model.getFilteredTaskList().size()));
+	}
+
+	public void updateByArchivedKeyword() {
+		model.updateArchivedFilteredTaskListByKeyword(archive);
+	}
+
+	public void updateByKeywords() {
+		model.updateFilteredTaskListByKeywords(keywords);
+	}
 
 	private void updateByStatusLevel(Status status) {
-		if(this.status.toString().equals(DONE)){
+		if (this.status.toString().equals(DONE)) {
 			model.updateFilteredTaskListByDoneStatus();
-		}
-		else if (this.status.toString().equals(UNDONE)){
+		} else if (this.status.toString().equals(UNDONE)) {
 			model.updateFilteredTaskListByUnDoneStatus();
-		}
-		else {
+		} else {
 			Assert.fail("unable to execute FindCommand due to incorrect Status");
 		}
 	}
 
 	private void updateByPriorityLevel(Priority priority) {
-		if(this.priority.toString().equals(HIGH_PRIORITY)) {
-            model.updateFilteredTaskListByHighPriority();
-        } else if(this.priority.toString().equals(MEDIUM_PRIORITY)) {
-            model.updateFilteredTaskListByMediumPriority();
-        } else if(this.priority.toString().equals(LOW_PRIORITY)) {
-            model.updateFilteredTaskListByLowPriority();
-        } else {
-            Assert.fail("unable to execute FindCommand due to incorrect Priority");
-        }
+		if (this.priority.toString().equals(HIGH_PRIORITY)) {
+			model.updateFilteredTaskListByHighPriority();
+		} else if (this.priority.toString().equals(MEDIUM_PRIORITY)) {
+			model.updateFilteredTaskListByMediumPriority();
+		} else if (this.priority.toString().equals(LOW_PRIORITY)) {
+			model.updateFilteredTaskListByLowPriority();
+		} else {
+			Assert.fail("unable to execute FindCommand due to incorrect Priority");
+		}
 	}
 
 }
--- src/main/java/seedu/address/model/ModelManager.java
+++ src/main/java/seedu/address/model/ModelManager.java
@@ -16,248 +16,274 @@ import seedu.address.model.Task.UniqueTaskList;
 import seedu.address.model.Task.UniqueTaskList.TaskNotFoundException;
 
 /**
- * Represents the in-memory model of the task manager data.
- * All changes to any model should be synchronized.
+ * Represents the in-memory model of the task manager data. All changes to any
+ * model should be synchronized.
  */
 public class ModelManager extends ComponentManager implements Model {
-    private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
+	private static final Logger logger = LogsCenter.getLogger(ModelManager.class);
 
-    private TaskManager taskManager;
-    private TaskManager previousTaskMgr;
-    private final FilteredList<ReadOnlyTask> filteredTasks;
+	private TaskManager taskManager;
+	private TaskManager previousTaskMgr;
+	private final FilteredList<ReadOnlyTask> filteredTasks;
 
-    /**
-     * Initializes a ModelManager with the given taskManager and userPrefs.
-     */
-    public ModelManager(ReadOnlyTaskManager taskManager, UserPrefs userPrefs) {
-        super();
-        assert !CollectionUtil.isAnyNull(taskManager, userPrefs);
+	/**
+	 * Initializes a ModelManager with the given taskManager and userPrefs.
+	 */
+	public ModelManager(ReadOnlyTaskManager taskManager, UserPrefs userPrefs) {
+		super();
+		assert !CollectionUtil.isAnyNull(taskManager, userPrefs);
 
-        logger.fine("Initializing with task manager: " + taskManager + " and user prefs " + userPrefs);
+		logger.fine("Initializing with task manager: " + taskManager + " and user prefs " + userPrefs);
 
-        this.taskManager = new TaskManager(taskManager);
+		this.taskManager = new TaskManager(taskManager);
 
+		filteredTasks = new FilteredList<>(this.taskManager.getTaskList());
 
-        filteredTasks = new FilteredList<>(this.taskManager.getTaskList());
-        
-        filteredTasks.setPredicate(task -> {
-            if(task.getStatus().toString().contains("un")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
+		filteredTasks.setPredicate(task -> {
+			if (task.getStatus().toString().contains("un")) {
+				return true;
+			} else {
+				return false;
+			}
+		});
 
-    }
+	}
 
-    public ModelManager() {
-        this(new TaskManager(), new UserPrefs());
-    }
+	public ModelManager() {
+		this(new TaskManager(), new UserPrefs());
+	}
 
-    @Override
-    public void resetData(ReadOnlyTaskManager newData) {
-        taskManager.resetData(newData);
-        indicateTaskManagerChanged();
-    }
+	@Override
+	public void resetData(ReadOnlyTaskManager newData) {
+		taskManager.resetData(newData);
+		indicateTaskManagerChanged();
+	}
 
-    @Override
-    public ReadOnlyTaskManager getTaskManager() {
-        return taskManager;
-    }
+	@Override
+	public ReadOnlyTaskManager getTaskManager() {
+		return taskManager;
+	}
 
-    /** Raises an event to indicate the model has changed */
-    private void indicateTaskManagerChanged() {
-        raise(new TaskManagerChangedEvent(taskManager));
-    }
+	/** Raises an event to indicate the model has changed */
+	private void indicateTaskManagerChanged() {
+		raise(new TaskManagerChangedEvent(taskManager));
+	}
 
-    @Override
-    public synchronized void deleteTask(ReadOnlyTask target) throws TaskNotFoundException {
-        setPrevious();
-        taskManager.removeTask(target);
+	@Override
+	public synchronized void deleteTask(ReadOnlyTask target) throws TaskNotFoundException {
+		setPrevious();
+		taskManager.removeTask(target);
 
-        indicateTaskManagerChanged();
-    }
+		indicateTaskManagerChanged();
+	}
 
-    @Override
-    public synchronized void addTask(Task task) throws UniqueTaskList.DuplicatetaskException {
-        setPrevious();
-        taskManager.addTask(task);
-        updateFilteredListToShowAll();
-        indicateTaskManagerChanged();
-    }
-    //@@author A0140072X
-    private void setPrevious(){
-        previousTaskMgr = new TaskManager(taskManager);
-    }
-    //@@author A0140072X
-    public boolean undoTask(){
-        if(previousTaskMgr == null) {
-            return false;
-        }
-        else {
-            TaskManager currentTaskMgr = new TaskManager(taskManager);
-            taskManager.resetData(previousTaskMgr);
-            previousTaskMgr.resetData(currentTaskMgr);
-            return true;
-        }
-        
-    }
-    
-    @Override
-    public void updateTask(int filteredTaskListIndex, ReadOnlyTask editedTask)
-            throws UniqueTaskList.DuplicatetaskException {
-        assert editedTask != null;
-        setPrevious();
+	@Override
+	public synchronized void addTask(Task task) throws UniqueTaskList.DuplicatetaskException {
+		setPrevious();
+		taskManager.addTask(task);
+		updateFilteredListToShowAll();
+		indicateTaskManagerChanged();
+	}
 
-        int taskManagerIndex = filteredTasks.getSourceIndex(filteredTaskListIndex);
-        taskManager.updateTask(taskManagerIndex, editedTask);
+	// @@author A0140072X
+	private void setPrevious() {
+		previousTaskMgr = new TaskManager(taskManager);
+	}
 
-        indicateTaskManagerChanged();
-    }
+	// @@author A0140072X
+	public boolean undoTask() {
+		if (previousTaskMgr == null) {
+			return false;
+		} else {
+			TaskManager currentTaskMgr = new TaskManager(taskManager);
+			taskManager.resetData(previousTaskMgr);
+			previousTaskMgr.resetData(currentTaskMgr);
+			return true;
+		}
 
-    //=========== Filtered Person List Accessors =============================================================
+	}
 
-    @Override
-    public UnmodifiableObservableList<ReadOnlyTask> getFilteredTaskList() {
-        return new UnmodifiableObservableList<>(filteredTasks);
-    }
+	@Override
+	public void updateTask(int filteredTaskListIndex, ReadOnlyTask editedTask)
+			throws UniqueTaskList.DuplicatetaskException {
+		assert editedTask != null;
+		setPrevious();
 
-    @Override
-    public void updateFilteredListToShowAll() {
-        //filteredTasks.setPredicate(null);
-        filteredTasks.setPredicate(task -> {
-            if(task.getStatus().toString().contains("un")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
+		int taskManagerIndex = filteredTasks.getSourceIndex(filteredTaskListIndex);
+		taskManager.updateTask(taskManagerIndex, editedTask);
 
-    }
-    //@@author A0140072X
-    public void updateFilteredTaskListByArchived() {
-        updateFilteredListToShowAll();
-        filteredTasks.setPredicate(task -> {
-            if(!task.getStatus().toString().contains("undone")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
+		indicateTaskManagerChanged();
+	}
 
-    @Override
-    public void updateFilteredTaskListByKeywords(Set<String> keywords) {
-        updateFilteredTaskListByKeywords(new PredicateExpression(new NameQualifier(keywords)));
-    }
+	// =========== Filtered Person List Accessors
+	// =============================================================
 
-    private void updateFilteredTaskListByKeywords(Expression expression) {
-    	filteredTasks.setPredicate(null);
-        filteredTasks.setPredicate(expression::satisfies);
-    }
-    public void updateFilteredTaskListByHighPriority() {
-    	filteredTasks.setPredicate(task -> {
-            if(task.getPriority().toString().equals("h")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
-    
-    public void updateFilteredTaskListByMediumPriority() {
-    	filteredTasks.setPredicate(task -> {
-            if(task.getPriority().toString().equals("m")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
-    
-    public void updateFilteredTaskListByLowPriority() {
-    	filteredTasks.setPredicate(task -> {
-            if(task.getPriority().toString().equals("l")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
-    public void updateFilteredTaskListByDoneStatus(){
-        filteredTasks.setPredicate(task -> {
-            if(task.getStatus().toString().equals("done")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
+	@Override
+	public UnmodifiableObservableList<ReadOnlyTask> getFilteredTaskList() {
+		return new UnmodifiableObservableList<>(filteredTasks);
+	}
 
-    public void updateFilteredTaskListByUnDoneStatus(){
-        filteredTasks.setPredicate(task -> {
-            if(task.getStatus().toString().equals("undone")) {
-                return true;
-            } else {
-                return false;
-            }
-        });
-    }
-    //========== Inner classes/interfaces used for filtering =================================================
+	@Override
+	public void updateFilteredListToShowAll() {
+		// filteredTasks.setPredicate(null);
+		filteredTasks.setPredicate(task -> {
+			if (task.getStatus().toString().contains("un")) {
+				return true;
+			} else {
+				return false;
+			}
+		});
 
-    interface Expression {
-        boolean satisfies(ReadOnlyTask task);
-        String toString();
-    }
+	}
 
-    private class PredicateExpression implements Expression {
+	// @@author A0140072X
+	public void updateFilteredTaskListByArchived() {
+		updateFilteredListToShowAll();
+		filteredTasks.setPredicate(task -> {
+			if (!task.getStatus().toString().contains("undone")) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 
-        private final Qualifier qualifier;
+	@Override
+	public void updateFilteredTaskListByKeywords(Set<String> keywords) {
+		updateFilteredTaskListByKeywords(new PredicateExpression(new NameQualifier(keywords)));
+	}
 
-        PredicateExpression(Qualifier qualifier) {
-            this.qualifier = qualifier;
-        }
+	private void updateFilteredTaskListByKeywords(Expression expression) {
+		filteredTasks.setPredicate(expression::satisfies);
+	}
 
-        @Override
-        public boolean satisfies(ReadOnlyTask task) {
-            return qualifier.run(task);
-        }
+	@Override
+	public void updateFilteredTaskListByHighPriority() {
+		filteredTasks.setPredicate(task -> {
+			if ((task.getPriority().toString().equals("h")) && (task.getStatus().toString().equals("undone"))) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 
-        @Override
-        public String toString() {
-            return qualifier.toString();
-        }
-    }
+	@Override
+	public void updateFilteredTaskListByMediumPriority() {
+		filteredTasks.setPredicate(task -> {
+			if ((task.getPriority().toString().equals("m")) && (task.getStatus().toString().equals("undone"))) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 
-    interface Qualifier {
-        boolean run(ReadOnlyTask task);
-        String toString();
-    }
+	@Override
+	public void updateFilteredTaskListByLowPriority() {
+		filteredTasks.setPredicate(task -> {
+			if ((task.getPriority().toString().equals("l")) && (task.getStatus().toString().equals("undone"))) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
 
-    private class NameQualifier implements Qualifier {
-        private Set<String> nameKeyWords;
+	@Override
+	public void updateFilteredTaskListByDoneStatus() {
+		filteredTasks.setPredicate(task -> {
+			if (task.getStatus().toString().equals("done")) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
+	
+	@Override
+	public void updateFilteredTaskListByUnDoneStatus() {
+		filteredTasks.setPredicate(task -> {
+			if (task.getStatus().toString().equals("undone")) {
+				return true;
+			} else {
+				return false;
+			}
+		});
+	}
+	
+	@Override
+	public void updateArchivedFilteredTaskListByKeyword(String archive) {
+		filteredTasks.setPredicate(task -> {
+			if (((StringUtil.containsWordIgnoreCase(task.getName().fullName, archive))
+					|| (StringUtil.containsWordIgnoreCase(task.getDescription().description, archive))
+					|| (StringUtil.containsTagIgnoreCase(task.getTags(), archive)))
+					&& (task.getStatus().toString().equalsIgnoreCase("done")))
+				return true;
+			else {
+				return false;
+			}
+		});
+	}
+	// ========== Inner classes/interfaces used for filtering
+	// =================================================
 
-        NameQualifier(Set<String> nameKeyWords) {
-            this.nameKeyWords = nameKeyWords;
-        }
+	interface Expression {
+		boolean satisfies(ReadOnlyTask task);
 
-        @Override
+		String toString();
+	}
 
-        public boolean run(ReadOnlyTask task) {
-        	for(String keyword: nameKeyWords){
-            	if((StringUtil.containsWordIgnoreCase(task.getName().fullName,keyword))
-    			|| (StringUtil.containsWordIgnoreCase(task.getDescription().description,keyword))
-    			|| (StringUtil.containsTagIgnoreCase(task.getTags(),keyword)))
-            		return true;
-            }
-            return false;
+	private class PredicateExpression implements Expression {
 
-        }
+		private final Qualifier qualifier;
 
-        @Override
-        public String toString() {
-            return "name=" + String.join(", ", nameKeyWords);
-        }
-    }
+		PredicateExpression(Qualifier qualifier) {
+			this.qualifier = qualifier;
+		}
+
+		@Override
+		public boolean satisfies(ReadOnlyTask task) {
+			return qualifier.run(task);
+		}
+
+		@Override
+		public String toString() {
+			return qualifier.toString();
+		}
+	}
+
+	interface Qualifier {
+		boolean run(ReadOnlyTask task);
+
+		String toString();
+	}
+
+	private class NameQualifier implements Qualifier {
+		private Set<String> nameKeyWords;
+
+		NameQualifier(Set<String> nameKeyWords) {
+			this.nameKeyWords = nameKeyWords;
+		}
+
+		@Override
+
+		public boolean run(ReadOnlyTask task) {
+			for (String keyword : nameKeyWords) {
+				if (((StringUtil.containsWordIgnoreCase(task.getName().fullName, keyword))
+						|| (StringUtil.containsWordIgnoreCase(task.getDescription().description, keyword))
+						|| (StringUtil.containsTagIgnoreCase(task.getTags(), keyword)))
+						&& (task.getStatus().toString().equalsIgnoreCase("undone")))
+					return true;
+			}
+			return false;
+
+		}
+
+		@Override
+		public String toString() {
+			return "name=" + String.join(", ", nameKeyWords);
+		}
+	}
 
 }
