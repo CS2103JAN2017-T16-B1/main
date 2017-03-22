8eaf337c30e600697271d1dbf1892ebe8933ff07
alwinsonauyong <alwinsonauyong@hotmail.com>
2017-03-22 14:42:07 +0800

    Update ArchivedCommand.java

--- src/main/java/seedu/address/logic/commands/ArchivedCommand.java
+++ src/main/java/seedu/address/logic/commands/ArchivedCommand.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 
 import seedu.address.model.Task.Priority;
 import seedu.address.model.tag.Tag;
+//@@author A0140072X
 /**
  * Lists all archived tasks in task manager.
  * 
