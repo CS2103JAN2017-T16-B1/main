620251b8a368137910917d8f3897006800a8c796
EDWARD <E0003394@u.nus.edu>
2017-03-29 18:55:05 +0800

    Beautified the display of task details

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -7,6 +7,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import seedu.address.model.Task.ReadOnlyTask;
 
+
 public class PersonCard extends UiPart<Region> {
 
     private static final String FXML = "PersonListCard.fxml";
@@ -25,18 +26,55 @@ public class PersonCard extends UiPart<Region> {
     private Label address;
     @FXML
     private FlowPane tags;
+    //@FXML
+    //private ImageView icon;
 
     public PersonCard(ReadOnlyTask task, int displayedIndex) {
         super(FXML);
         name.setText(task.getName().fullName);
         id.setText(displayedIndex + ". ");
-        phone.setText("Description : " + task.getDescription().description);
-        email.setText("Start Time : " + task.getStartTime().startTime);
+        setTextForDescription(task);
+        setTextForStartTime(task);
+        setTextForEndTime(task);
         address.setText("End Time : " +task.getEndTime().endTime);
         initTags(task);
+        //setColours(task);
     }
 
-    private void initTags(ReadOnlyTask task) {
+    /*private void setColours(ReadOnlyTask task) {
+		if((task.getStartTime().toString().equals("\n") && task.getEndTime().toString().equals("\n"))){
+			name.getStyleClass().add("label-red");
+			//icon.setImage(new Image("/images/GUI.PNG"));
+			//icon.setImage(null);
+		}
+		
+	}*/
+
+	private void setTextForEndTime(ReadOnlyTask task) {
+		if(!(task.getEndTime().endTime.equals(""))){
+			address.setText("End Time : " +task.getEndTime().endTime);
+        } else if(task.getStartTime().startTime.equals("")){
+        	address.setManaged(false);
+        }
+	}
+
+	private void setTextForStartTime(ReadOnlyTask task) {
+		if(!(task.getStartTime().startTime.equals(""))){
+        	email.setText("Start Time : " + task.getStartTime().startTime);
+        } else if(task.getStartTime().startTime.equals("")){
+        	email.setManaged(false);
+        }
+	}
+
+	private void setTextForDescription(ReadOnlyTask task) {
+		if(!(task.getDescription().description.equals("\n"))){
+        	phone.setText("Description : " + task.getDescription().description);
+        } else if(task.getDescription().description.equals("\n")){
+        	phone.setManaged(false);
+        }
+	}
+
+	private void initTags(ReadOnlyTask task) {
         task.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
     }
 }
