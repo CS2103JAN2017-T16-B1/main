efad9426a66af89d60d67dacbf98c89385c424d7
EDWARD <E0003394@u.nus.edu>
2017-03-30 15:06:17 +0800

    added more sample data

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -27,15 +27,20 @@ public class SampleDataUtil {
 
 
             		new Task(new Name("buy milk"), new Description("87438807"), new StartTime(""),
-                            new EndTime("2017-03-04-1000"), new ID("1"), new Priority("h"), new Status("undone"),
+                            new EndTime("2017-03-04-1000"), new ID("1"), new Priority("l"), new Status("undone"),
                             new RecurPeriod(""), new RecurEndDate (""), new UniqueTagList("friends")),
             		new Task(new Name("buy house"), new Description("87438807"), new StartTime(""),
                             new EndTime("2017-03-04-1000"), new ID("1"), new Priority("h"), new Status("undone"), 
                             new RecurPeriod(""), new RecurEndDate (""), new UniqueTagList("friends")),
             		new Task(new Name("study"), new Description("87438807"), new StartTime(""),
+                            new EndTime("2017-03-04-1000"), new ID("1"), new Priority("m"), new Status("undone"),
+                            new RecurPeriod(""), new RecurEndDate (""),new UniqueTagList("friends")),
+            		new Task(new Name("Buy more milk"), new Description("call boss at 87438807"), new StartTime("2016-03-04-1500"),
+                            new EndTime("2017-03-04-1600"), new ID("3"), new Priority("h"), new Status("undone"),
+                            new RecurPeriod(""), new RecurEndDate (""),new UniqueTagList("friends")),
+            		new Task(new Name("review CS code"), new Description("after the tutorial"), new StartTime(""),
                             new EndTime("2017-03-04-1000"), new ID("1"), new Priority("h"), new Status("undone"),
                             new RecurPeriod(""), new RecurEndDate (""),new UniqueTagList("friends"))
-
             };
         } catch (IllegalValueException e) {
             throw new AssertionError("sample data cannot be invalid", e);
