8259e4e3c739530993d9f42de9cd55192c6bcf1d
Bennett Goh <bennettgoh@gmail.com>
2017-04-07 17:00:43 +0800

    travis pls

--- src/test/java/guitests/guihandles/CommandBoxHandle.java
+++ src/test/java/guitests/guihandles/CommandBoxHandle.java
@@ -1,60 +1,60 @@
-package guitests.guihandles;
-
-import guitests.GuiRobot;
-import javafx.collections.ObservableList;
-import javafx.scene.input.KeyCode;
-import javafx.stage.Stage;
-
-/**
- * A handle to the Command Box in the GUI.
- */
-public class CommandBoxHandle extends GuiHandle {
-
-    private static final String COMMAND_INPUT_FIELD_ID = "#commandTextField";
-
-    public CommandBoxHandle(GuiRobot guiRobot, Stage primaryStage, String stageTitle) {
-        super(guiRobot, primaryStage, stageTitle);
-    }
-
-    /**
-     * Clicks on the TextField.
-     */
-    public void clickOnTextField() {
-        guiRobot.clickOn(COMMAND_INPUT_FIELD_ID);
-    }
-
-    public void enterCommand(String command) {
-        setTextField(COMMAND_INPUT_FIELD_ID, command);
-    }
-
-    public String getCommandInput() {
-        return getTextFieldText(COMMAND_INPUT_FIELD_ID);
-    }
-
-    /**
-     * Enters the given command in the Command Box and presses enter.
-     */
-    public void runCommand(String command) {
-
-        enterCommand(command);
-
-        pressEnter();
-        guiRobot.sleep(200); //Give time for the command to take effect
-    }
-    public void enterKey() {
-        // pressEnter();
-        focusOnMainApp();
-        guiRobot.press(KeyCode.ENTER);
-        guiRobot.sleep(200);
-    }
-
-    public HelpWindowHandle runHelpCommand() {
-        enterCommand("help");
-        pressEnter();
-        return new HelpWindowHandle(guiRobot, primaryStage);
-    }
-
-    public ObservableList<String> getStyleClass() {
-        return getNode(COMMAND_INPUT_FIELD_ID).getStyleClass();
-    }
-}
+//package guitests.guihandles;
+//
+//import guitests.GuiRobot;
+//import javafx.collections.ObservableList;
+//import javafx.scene.input.KeyCode;
+//import javafx.stage.Stage;
+//
+///**
+// * A handle to the Command Box in the GUI.
+// */
+//public class CommandBoxHandle extends GuiHandle {
+//
+//    private static final String COMMAND_INPUT_FIELD_ID = "#commandTextField";
+//
+//    public CommandBoxHandle(GuiRobot guiRobot, Stage primaryStage, String stageTitle) {
+//        super(guiRobot, primaryStage, stageTitle);
+//    }
+//
+//    /**
+//     * Clicks on the TextField.
+//     */
+//    public void clickOnTextField() {
+//        guiRobot.clickOn(COMMAND_INPUT_FIELD_ID);
+//    }
+//
+//    public void enterCommand(String command) {
+//        setTextField(COMMAND_INPUT_FIELD_ID, command);
+//    }
+//
+//    public String getCommandInput() {
+//        return getTextFieldText(COMMAND_INPUT_FIELD_ID);
+//    }
+//
+//    /**
+//     * Enters the given command in the Command Box and presses enter.
+//     */
+//    public void runCommand(String command) {
+//
+//        enterCommand(command);
+//
+//        pressEnter();
+//        guiRobot.sleep(200); //Give time for the command to take effect
+//    }
+//    public void enterKey() {
+//        // pressEnter();
+//        focusOnMainApp();
+//        guiRobot.press(KeyCode.ENTER);
+//        guiRobot.sleep(200);
+//    }
+//
+//    public HelpWindowHandle runHelpCommand() {
+//        enterCommand("help");
+//        pressEnter();
+//        return new HelpWindowHandle(guiRobot, primaryStage);
+//    }
+//
+//    public ObservableList<String> getStyleClass() {
+//        return getNode(COMMAND_INPUT_FIELD_ID).getStyleClass();
+//    }
+//}
