78a285823d1e6153897099328026192eb6271f44
Bennett Goh <bennettgoh@gmail.com>
2017-04-09 22:36:13 +0800

    no message

--- src/test/java/guitests/guihandles/TaskListPanelHandle.java
+++ src/test/java/guitests/guihandles/TaskListPanelHandle.java
@@ -62,7 +62,6 @@ public class TaskListPanelHandle extends GuiHandle {
                     "Expected " + (getListView().getItems().size() - 1) + " tasks");
         }
 
-        
         for (int i = 0; i < tasks.length; i++) {
             final int scrollTo = i + startPosition;
             guiRobot.interact(() -> getListView().scrollTo(scrollTo));
